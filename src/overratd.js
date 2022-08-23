a02b70c16e12c6682843496c0e64cc4d3b5284df
Garret Lowe <garret.lowe98@gmail.com>
2022-08-23 17:27:41 -0400

    Added License

--- src/overratd.js
+++ src/overratd.js
@@ -1,3 +1,21 @@
+/*
+ * Overratd | Adds rating histograms to public lists on Letterboxd
+ * Copyright (C) 2022  Garret Lowe
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 function getElementByXPath(xpath) 
 {
     return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
